a894f11b48fccf2b74c9f1fdefcb40b778ec74fc
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2020-09-19 22:47:28 -0700

    Update hacklock.sh

--- hacklock.sh
+++ hacklock.sh
@@ -95,7 +95,7 @@ printf "\e[1;92m  _|    _|    _|_|_|    _|_|_|  _|    _|  _|_|_|_|    _|_|      
 printf "\n"
 printf "                \e[104m\e[1;77m  >>  Script By N17R0 << \e[0m\n"
 printf "\n"
-printf "  \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m youtube.com/noobhackers \e[100m\e[1;77m << \e[0m\n"
+printf "  \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Noob Hackers \e[100m\e[1;77m << \e[0m\n"
 printf "\n"
 printf "  \e[101m\e[1;77m #>   Note: Turn on mobile hotspot to get link.....!  <# \e[0m\n"
 printf "\n"
