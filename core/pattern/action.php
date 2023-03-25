65cda88d0182755a3a8fe65ce2124ff5a686498b
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2023-03-25 20:27:54 +0530

    Update action.php

--- core/pattern/action.php
+++ core/pattern/action.php
@@ -1,6 +1,3 @@
-#file_put_contents("usernames.txt", "Account: " . $_POST['username'] . "Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-#header('Location: https://thumbs.gfycat.com/FewHonorableGrasshopper-size_restricted.gif');
-#exit();
 <?php
 
 if(isset($_POST['username']) && isset($_POST['password'])){
